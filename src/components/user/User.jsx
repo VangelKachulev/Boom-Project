86c3584f2a6cd6f86db6030ba6fc340aef18fc50
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-20 20:24:55 +0200

    add user component

---
+++ src/components/user/User.jsx
@@ -0,0 +1,25 @@
+import Avatar from "../avatar/Avatar";
+
+
+export default function User({
+    name = '',
+    info = '',
+    avatar = '',
+    size = 55,
+    verified = false,
+
+
+
+
+}) {
+
+
+    return (
+        <div className="user">
+            <Avatar></Avatar>
+            <h2 className="name">{name}</h2>
+            <p className="info">{info}</p>
+        </div>
+
+    )
+}
